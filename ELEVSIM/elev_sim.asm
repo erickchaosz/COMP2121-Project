11ac85c5aa5fc09fff32efbec82290e03069445f
JamesAlexander1 <jamesbrooksalexander@gmail.com>
2015-10-22 16:06:00 +1100

    lcd is acting funny

--- ELEVSIM/elev_sim.asm
+++ ELEVSIM/elev_sim.asm
@@ -13,12 +13,36 @@ TempCounter:
 ; macros
 ;==================================
 
+.macro do_lcd_command
+        ldi r16, @0
+        rcall lcd_command
+        rcall lcd_wait
+.endmacro
+.macro do_lcd_data
+        mov r16, @0
+        rcall lcd_data
+        rcall lcd_wait
+.endmacro
+
+
 .macro clear
   ldi YL, low(@0)
   ldi YH, high(@0)
   clr temp1
   st Y+, temp1
   st Y, temp1
+.endmacro
+
+.equ LCD_RS = 7
+.equ LCD_E = 6
+.equ LCD_RW = 5
+.equ LCD_BE = 4
+
+.macro lcd_set
+        sbi PORTA, @0
+.endmacro
+.macro lcd_clr
+        cbi PORTA, @0
 .endmacro
 
 
@@ -69,7 +93,8 @@ RESET:
   clear SecondCounter
   ;
   ldi QueueCtr, 0
-  ;
+
+  ;KEYPAD init.
   ldi temp1, 0b00000000
   out TCCR0A, temp1
   ldi temp1, 0b00000010
@@ -83,7 +108,29 @@ RESET:
   out DDRC, temp1
   out PORTC, temp1
   clr acc
+  ser r16
 
+  ;LCD init.
+  out DDRF, r16
+  out DDRA, r16
+  clr r16
+  out PORTF, r16
+  out PORTA, r16
+
+  do_lcd_command 0b00111000 ; 2x5x7
+  rcall sleep_5ms
+  do_lcd_command 0b00111000 ; 2x5x7
+  rcall sleep_1ms
+  do_lcd_command 0b00111000 ; 2x5x7
+  do_lcd_command 0b00111000 ; 2x5x7
+  do_lcd_command 0b00001000 ; display off?
+  do_lcd_command 0b00000001 ; clear display
+  do_lcd_command 0b00000110 ; increment, no display shift
+  do_lcd_command 0b00001110 ; Cursor on, bar, no blink
+
+  do_lcd_command 0b11000000
+  do_lcd_data r16
+	
 main:
   ldi cmask, INITCOLMASK  ; initial column mask
   clr col                 ; initial column
@@ -147,10 +194,10 @@ convert:
   cpi QueueCtr, 200
   breq FULLQueue
   
-  
+  push r16
   st z+, temp1
-
-  
+  do_lcd_data temp1
+  pop r16
   inc QueueCtr
 
 convert_jump:
@@ -237,4 +284,72 @@ EndIF:
   pop temp1
   reti
 
+
+        ;;
+        ;;  Send a command to the LCD (r16)
+        ;;
+
+lcd_command:
+        out PORTF, r16
+        rcall sleep_1ms
+        lcd_set LCD_E
+        rcall sleep_1ms
+        lcd_clr LCD_E
+        rcall sleep_1ms
+        ret
+
+lcd_data:
+        out PORTF, r16
+        lcd_set LCD_RS
+        rcall sleep_1ms
+        lcd_set LCD_E
+        rcall sleep_1ms
+        lcd_clr LCD_E
+        rcall sleep_1ms
+        lcd_clr LCD_RS
+        ret
+
+lcd_wait:
+        push r16
+        clr r16
+        out DDRF, r16
+        out PORTF, r16
+        lcd_set LCD_RW
+lcd_wait_loop:
+        rcall sleep_1ms
+        lcd_set LCD_E
+        rcall sleep_1ms
+        in r16, PINF
+        lcd_clr LCD_E
+        sbrc r16, 7
+        rjmp lcd_wait_loop
+        lcd_clr LCD_RW
+        ser r16
+        out DDRF, r16
+        pop r16
+        ret
+
+.equ F_CPU = 16000000
+.equ DELAY_1MS = F_CPU / 4 / 1000 - 4
+        ;;  4 cycles per iteration - setup/call-return overhead
+
+sleep_1ms:
+        push r24
+        push r25
+        ldi r25, high(DELAY_1MS)
+        ldi r24, low(DELAY_1MS)
+delayloop_1ms:
+        sbiw r25:r24, 1
+        brne delayloop_1ms
+        pop r25
+        pop r24
+        ret
+
+sleep_5ms:
+        rcall sleep_1ms
+        rcall sleep_1ms
+        rcall sleep_1ms
+        rcall sleep_1ms
+        rcall sleep_1ms
+        ret
   
